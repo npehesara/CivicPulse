2a027d00f57c7da6ca7e26b30c1bbb9479bbbaa1
npehesara <swnavodpehesara@gmail.com>
2026-09-01 15:24:09 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/civicpulse_backend/CivicpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package civicpulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicpulseBackendApplication.class, args);
+	}
+
+}
